93ead443bf8b6d305ff5922b79ee42c43c5cc1cb
Nastya N <asrozova@gmail.com>
2023-12-13 01:02:22 +0100

    feat: header

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header>
+        <div>
+            <img src="../assets/logo.svg" alt="logo" />
+            <MainButton />
+        </div>
+    </header>
+</template>
+
+<script>
+import MainButton from "./MainButton.vue"
+export default {
+    name: "app-header",
+    components: {
+        MainButton
+    }
+}
+</script>
+
+<style scoped>
+header {
+    box-shadow: 0 0 12px rgba(0, 0, 0, 0.18);
+    width: 100vw;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    position: fixed;
+    z-index: 100;
+    justify-content: center;
+    background-color: white;
+
+    div {
+        width: calc(100vw - 80px);
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+        img {
+            height: 40px;
+            transition: opacity 0.3s ease-in-out;
+            cursor: pointer;
+
+            &:hover {
+                opacity: 0.7;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/MainButton.vue
@@ -0,0 +1,81 @@
+<template>
+    <button>
+        <p>Add Post</p>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "main-button",
+
+}
+</script>
+
+<style scope>
+button {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    border-radius: 16px;
+    background: radial-gradient(circle farthest-corner at 100% -25%, white 5%, #92e4e6, #4ca1af 35%);
+    padding: 16px 24px;
+    border: none;
+    color: white;
+    cursor: pointer;
+    line-height: 0;
+    display: flex;
+    align-items: center;
+    z-index: 150;
+
+    p {
+        z-index: 161;
+        margin: 0;
+    }
+
+    &::before {
+        position: absolute;
+        content: "";
+        inset: 0;
+        z-index: 149;
+        background: radial-gradient(circle farthest-corner at 0% 0%, white 5%, #92e4e6, #4ca1af 35%);
+        opacity: 0;
+        transition: opacity 0.25s linear;
+        margin: 0;
+        padding: 0;
+        border: 0;
+        border-radius: 16px;
+        padding: 16px 24px;
+        border: none;
+        color: white;
+        cursor: pointer;
+        line-height: 0;
+        display: flex;
+        align-items: center;
+    }
+
+    &:hover::before {
+        opacity: 1;
+    }
+
+    &:after {
+        content: "";
+        background: radial-gradient(circle farthest-corner at 0% 0%, white 5%, #92e4e6, #4ca1af 95%);
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        opacity: 0;
+        transition: all 0.8s;
+        z-index: 160;
+        border-radius: 16px;
+    }
+
+    &:active:after {
+        opacity: 1;
+        transition: 0s
+    }
+}
+</style>
